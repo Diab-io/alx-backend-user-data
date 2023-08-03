58fcaac8bdb6120d7397adff816ee45b74ce148d
Odimayo David <odimayodavid7@gmail.com>
2023-08-03 02:29:35 +0100

    Updated code

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -6,8 +6,7 @@ from typing import List
 
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str):
-    """Used to filter sensitive info
-    """
+    """Used to filter sensitive info"""
     pattern = r"(" + "|".join(map(re.escape, fields))\
         + r")=([^" + re.escape(separator) + r"]+)"
     return re.sub(pattern, r"\1=" + redaction, message)
