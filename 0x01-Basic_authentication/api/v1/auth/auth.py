1e3ca79bc9db3c001f567c013fe19a93948c5afe
Odimayo David <odimayodavid7@gmail.com>
2023-08-07 18:19:47 +0100

    Added conditions for checking if authentication is required for a route

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -11,12 +11,23 @@ class Auth:
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if a view requires authentication
         """
+        if path is None:
+            return True
+        if excluded_paths is None or not excluded_paths:
+            return True
+        if path and path[-1] != '/':
+            path += '/'
+        if path in excluded_paths:
+            return False
+        if path not in excluded_paths:
+            return True
         return False
 
     def authorization_header(self, request=None) -> str:
         """ Method that handles processes in the authorization header
         """
         return None
+
     def current_user(self, request=None) -> TypeVar('User'):
         """ Method that gets current user if authenticated
         """
