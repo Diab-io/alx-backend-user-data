dfd87046228952d08e1f793700aa6132131cb828
Odimayo David <odimayodavid7@gmail.com>
2023-08-16 02:55:39 +0100

    Updated db.py and turned off echo while creating engine

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -18,7 +18,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db", echo=False)
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -35,14 +35,11 @@ class DB:
     def add_user(self, email: str, hashed_password: str) -> User:
         """Add user to the database
         """
-        try:
-            new_user = User(email=email, hashed_password=hashed_password)
-            self._session.add(new_user)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            new_user = None
-        return new_user
+        user_obj = User(email=email, hashed_password=hashed_password)
+        if not self.__session:
+            self._session
+        self.__session.add(user_obj)
+        self.__session.commit()
 
         return user_obj
 
