b0713e460bf122de226bab9e1a51984166207016
Odimayo David <odimayodavid7@gmail.com>
2023-08-16 17:23:53 +0100

    Added reset_password route

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -75,5 +75,18 @@ def profile():
     return jsonify({"email": user.email})
 
 
+@app.route('/reset_password', methods=['POST'], strict_slashes=False)
+def reset_password():
+    """Resets password
+    """
+    email = request.form.get('email')
+    try:
+        user = AUTH._db.find_user_by(email=email)
+    except Exception:
+        abort(403)
+    reset_token = AUTH.get_reset_password_token(user.email)
+    return jsonify({"email": "<user email>", "reset_token": reset_token})
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
