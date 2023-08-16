4db47fa520355865bd82b2684999e8b2d866e9af
Odimayo David <odimayodavid7@gmail.com>
2023-08-16 14:51:27 +0100

    Created a login route that logs a user in and stores the session in a cookie

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """ A simple flask app
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort, session
 from auth import Auth
 
 AUTH = Auth()
@@ -20,7 +20,7 @@ def users():
     """Returns a simple payload
     """
     email = request.form.get('email')
-    password = request.form.get('email')
+    password = request.form.get('password')
     if not email or not password:
         return
     try:
@@ -28,6 +28,25 @@ def users():
         return jsonify({"email": user_obj.email, "message": "user created"})
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route('/sessions', methods=['POST'], strict_slashes=False)
+def login():
+    """Implementation of login
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    if not email or not password:
+        return
+    valid = AUTH.valid_login(email, password)
+    if not valid:
+        abort(401)
+    user_session_id = AUTH.create_session(email)
+
+    response = jsonify({"email": email, "message": "logged in"})
+    response.set_cookie('session_id', user_session_id)
+
+    return response
 
 
 if __name__ == "__main__":
