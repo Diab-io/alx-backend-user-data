d97f956377a955adfcf9f68f351f0c0a4fd357dc
Odimayo David <odimayodavid7@gmail.com>
2023-08-16 03:07:43 +0100

    Implemented a method that registers a user and also handles duplicate

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -2,6 +2,9 @@
 """Auth module
 """
 from bcrypt import hashpw, gensalt
+from db import DB
+from user import User
+from sqlalchemy.orm.exc import NoResultFound
 
 
 def _hash_password(password: str) -> bytes:
@@ -9,3 +12,22 @@ def _hash_password(password: str) -> bytes:
     """
     pass_hash = hashpw(password=password.encode('utf-8'), salt=gensalt())
     return pass_hash
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+    def register_user(self, email: str, password: str) -> User:
+        """Registers a user
+        """
+        try:
+            user_check = self._db.find_user_by(email=email)
+        except NoResultFound:
+            hash_pass = _hash_password(password)
+            user_obj = self._db.add_user(email, hash_pass)
+            return user_obj
+        raise ValueError("User <user's email> already exists")
